ec635fc45c0d3750b7a72eabd7cace239b49f928
Scott Dean <sdean82@gmail.com>
2024-12-14 09:00:04 -0600

    Initial Commit

---
+++ AoC2024/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  AoC2024
+//
+//  Created by Scott Dean on 12/14/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
