b03c3c6c27d11e8099db938cd85b213e27440375
Scott Dean <sdean82@gmail.com>
2024-12-15 07:55:05 -0600

    Day 1 complete

--- AoC2024/Day1/main.c
+++ AoC2024/Day1/main.c
@@ -11,6 +11,7 @@
 // Define a structure to hold each part of numbers (representing a row)
 int col1[1000];
 int col2[1000];
+int similarity_score;
 
 // Comparison function for qsort
 int compare(const void *a, const void *b) {
@@ -35,7 +36,7 @@ FILE* FileOpen(const char* filename, const char* mode) {
     return file;
 }
 int main(int argc, const char * argv[]) {
-    int n = 0, i = 0, sum = 0;
+    int i = 0, sum = 0;
     
     // Open the input file
     FILE *infile = FileOpen("/Users/scott/Documents/AoC2024/AoC2024/input.txt", "r");
@@ -47,7 +48,7 @@ int main(int argc, const char * argv[]) {
     
     // Close the file
     fclose(infile);
-    n = sizeof(col1) / sizeof(col1[0]);
+    int n = sizeof(col1) / sizeof(col1[0]);
     
     // Sort the array based on teh first column using qsort
     qsort(col1, n, sizeof(int), compare);
@@ -56,12 +57,24 @@ int main(int argc, const char * argv[]) {
     // Output the result
     size_t Length = sizeof(col1) / sizeof(col1[0]);
     
-    FILE *outfile = FileOpen("/Users/scott/Documents/AoC2024/AoC2024/output.txt", "w");
+    // FILE *outfile = FileOpen("/Users/scott/Documents/AoC2024/AoC2024/output.txt", "w");
     for (i=0; i < Length; i++) {
-        fprintf(outfile, "%d %d\n", col1[i], col2[i]);
+        // fprintf(outfile, "%d %d\n", col1[i], col2[i]);
         sum += abs(col1[i]-col2[i]);
     }
-    fclose(outfile);
+    int appears = 0, totals = 0;
+    for (i=0; i<Length; i++) {
+        appears = 0;
+        for(int y=0; y<Length; y++){
+            if (col1[i]==col2[y]) {
+                appears++;
+            }
+            totals = col1[i] * appears;
+        }
+        similarity_score += totals;
+    }
+    // fclose(outfile);
     printf("The total distance between the lists are:%d\n", sum);
+    printf("The similarity score is:%d\n", similarity_score);
     return 0;
 }
