7c00023e1355e3dca7c9b384a68944a925c69002
Scott Dean <sdean82@gmail.com>
2024-12-22 09:05:54 -0600

    Day 2 part 2 completed

--- AoC2024/Day2/Day2.c
+++ AoC2024/Day2/Day2.c
@@ -34,7 +34,13 @@ bool is_safe_report(int *report, int size) {
 int main (int argc, char** argv) {
     
     struct Report curReport;
-    int SafeReports= 0;
+    struct Report tmpReport;
+    int safeReports = 0;
+    int skip = 0;
+    char *line = NULL;  // Pointer for storing the line
+    size_t len = 0;     // Size of the line buffer
+    ssize_t nread;      // Number of characters read
+    char *saveptr;      // Store context between calls
     
     // Open the file of input
     FILE *file = fopen("/Users/scott/Documents/AoC2024/AoC2024/Day2/day2_input.txt", "r");
@@ -43,17 +49,11 @@ int main (int argc, char** argv) {
         return -1;
     }
     
-    char *line = NULL;  // Pointer for storing the line
-    size_t len = 0;     // Size of the line buffer
-    ssize_t nread;      // Number of characters read
-    
-    char *saveptr;      // Store context between calls
-    
     // Loop through file and report number of lines read and number of characters read
     while ((nread = getline(&line, &len, file)) != -1) {
         
         // Initialize structure report with zeros
-        memset(curReport.level, 0, sizeof(curReport.numlevels));
+        memset(curReport.level, 0, sizeof(curReport.level));
         curReport.numlevels=0;
         // Print the line read from the file
         printf("Retrieved line (%zd chars): %s", nread, line);
@@ -81,13 +81,50 @@ int main (int argc, char** argv) {
         }
         // Determine if the report is safe or not
         if (is_safe_report(curReport.level, curReport.numlevels)) {
-            printf("Report is safe\n");
-            ++SafeReports;
+            printf("Report is safe.\n");
+            ++safeReports;
         } else {
-            printf("Report is not safe\n");
+            // Check to see if removing one level from the report
+            // make the report safe
+            skip = 0;
+            // create a temp report minus one level
+            // This loops through X number of levels
+            for (int i = 0; i < curReport.numlevels; i++) {
+                memset(tmpReport.level, 0, sizeof(tmpReport.level));
+                tmpReport.numlevels=0;
+                // This copies number into the temp report
+                for (int n = 0; n < (curReport.numlevels); n++) {
+                    // if n is less than skip
+                    if (n < i) {
+                        tmpReport.level[n]= curReport.level[n];
+                        tmpReport.numlevels++;
+                        continue;
+                    }
+                    // if n is greater than skip
+                    if (n > i) {
+                        tmpReport.level[n-1] = curReport.level[n];
+                        tmpReport.numlevels++;
+                        continue;
+                    }
+                    // handle equal?
+                    else {
+
+                    }
+                }
+                if (is_safe_report(tmpReport.level, tmpReport.numlevels)) {
+                    ++safeReports;
+                    break;
+                }
+                // try again
+                skip++;
+            }
+            if (skip == curReport.numlevels) {
+                printf("Report is unsafe.\n");
+            }
+            printf("Report is safe when one level was removed.\n");
         }
     }
-    printf("The total number of safe repots is %d\n",SafeReports);
+    printf("The total number of safe repots is %d\n",safeReports);
     // Close the file
     fclose(file);
     
